f69db4f8627813c8b4c19dbfe4344fb11da3c7d5
Pragadeesh <rpraga50@gmail.com>
2019-07-31 00:09:38 +0530

    learn constructor

---
+++ student.java
@@ -0,0 +1,23 @@
+class CT{
+string name="ooo";
+int age=20 ,percent=77 ;
+   CT(string name,int age,int precent){
+   this.name=name;
+   this.age=age;
+   this percent=percent;
+   }
+   void display(){
+   System.out.println("name is :"+name+" age is :"+age);
+  }
+}
+class Student()
+{
+public static void main(String args[])
+CT stu1=new CT("praga",17,79);
+CT stu2=new CT("saba",18,80);
+CT stu3=new CT("prathap",18,60);
+stu1.display();
+stu2.display();
+stu3.display();
+}
+}
