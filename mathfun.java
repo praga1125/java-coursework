e0ad2f125768db0786af2a8a41e25eade19edce9
Pragadeesh <rpraga50@gmail.com>
2019-08-01 22:00:30 +0530

    learn maths functions

---
+++ mathfun.java
@@ -0,0 +1,7 @@
+class mathfun{
+   public static void main(String args[]){
+   int i=7;
+   System.out.println("the square root of "+i+"="+Math.sqrt(i));
+   System.out.println("2 power of "+i+"="+Math.pow(7,2));
+   }
+} 
